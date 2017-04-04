047931f00ebdc69b2baac2abaabc345a774992f3
Vishwajeet Narwal <vnarwal95@gmail.com>
2017-04-04 18:07:45 -0500

    GAE deployment settings

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from time import sleep
 from random import randint
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
-from tweepy import Stream 
+from tweepy import Stream
 from flask import jsonify
 from flask import request
 
@@ -31,60 +31,47 @@ def Go_bots():
 	hashtags_array = args.getlist('hashtags')
 	macro_link = args['macro_link']
 	tweet_text = args['tweet_text']
-	
-	
+
+
 	search_result_article = []
 	search_result_hashtag = []
+
 	for article in article_links_array:
-		search_result_article.append(api.search(article))
-	
+
+		try:
+			abc = api.search(article)
+			search_result_article.append(abc)
+		except Exception as e:
+			print e
+
+
 	for hashtag in hashtags_array:
 		search_result_hashtag.append(api.search('#'+hashtag))
 	all_tweets_text = []
-	
+
 	for tweets_a in search_result_article:
 		for t in tweets_a:
-		handle = "@" + t.user.screen_name
-		m_a = handle + " " + macro_link
-		all_tweets_text.append(m_a)
-		s = api.update_status(m_a)
-#		nap = randint(1, 60)
-		time.sleep(50)
-	
+			handle = "@" + t.user.screen_name
+			m_a = handle + " " + macro_link
+			all_tweets_text.append(m_a)
+			s = api.update_status(m_a)
+	#		nap = randint(1, 60)
+			time.sleep(50)
+
 	for tweets in search_result_hashtag:
 		for tweet in tweets:
 			handle = "@" + tweet.author.screen_name
 			m = handle + " " + macro_link
 			all_tweets_text.append(m)
-	##		s = api.update_status(m)
-	#		nap = randint(1, 60)
+			s = api.update_status(m)
+			nap = randint(1, 60)
 			time.sleep(50)
 
-### Streaming part commented
 
-	# name = 'bakerk200'
 
-	# class StdOutListener(StreamListener):
-		# search_result2 = api.search(name)
-		
-		# for t in search_result2:
-			# if(t.user.screen_name is name):
-				# handle2 = "@" + t.user.screen_name
-				# m2 = handle2 + " " + "good"
-				#####s2 = api.update_status(m2)
-				# nap = randint(1, 60)
-				# time.sleep(nap)
-
-	# l = StdOutListener()
-	# stream = Stream(auth, l)
-	# print(search_result_article)
-	# print(search_result_hashtag)
-	
-	
-	
 	return jsonify({"tweets": all_tweets_text})
-    
-   
+
+
 
 if(__name__) == '__main__':
     app.run(debug=True)
